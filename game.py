e520198fb6ba7c5f6799cc3aab72e1bd3c622ebe
csood2 <chaitanyasood01@gmail.com>
2020-12-25 13:59:36 -0500

    seems to work

--- game.py
+++ game.py
@@ -1,16 +1,22 @@
 import tkinter as tk
 from tkinter import *
 import random
+import copy
 
 
 value_matrix = [[0 for x in range(9)] for y in range(9)]
 button_matrix = [[None for x in range(9)] for y in range(9)]
-option_matrix = [[0 for x in range(3)] for y in range(4)]
+option_matrix1 = [[0 for x in range(3)] for y in range(4)]
+option_matrix2 = [[0 for x in range(3)] for y in range(4)]
+radio_list = [None for x in range(3)]
 root = tk.Tk()
 rad_val = tk.IntVar()
 rad_val.set(-1)
 turn_count = 0
-curr_options = option_matrix[0]
+#curr_options = option_matrix[0]
+curr_options1 = option_matrix1[0]
+curr_options2 = option_matrix2[0]
+curr_player = 1
 vert_labels = [None for x in range(9)]
 hor_labels = [None for x in range(9)]
 p1_score = ""
@@ -20,6 +26,7 @@ p2_target = ""
 score_labels = [None for x in range(2)]
 max_size = 9
 rounds = 4
+round_start_player = 1
 
 
 
@@ -48,7 +55,7 @@ def render():
 
     #display available options currently with radio
     #buttons which are updated with turns
-    display_options(curr_options, root)
+    display_new_round()
     B = tk.Button(root, text ="Hello")
     B.grid(row=12,column=12)
     B.config(command = temp)
@@ -70,6 +77,8 @@ def render():
         hor_labels[i] = lab
 
     #Create player score section
+    global p1_target
+    global p2_target
     foo = [0,1]
     if (random.choice(foo) == 0):
         p1_target = "even"
@@ -95,7 +104,7 @@ def render():
 
 
     freeze_all()
-    unfreeze_range(3)
+    unfreeze_range(3,6)
 
 
     root.mainloop()
@@ -110,11 +119,19 @@ def freeze_all():
 
 
 #unfreeze a specific range
-def unfreeze_range(x):
-    for a in looper(x):
-        button_matrix[a[0]][a[1]].configure(bg="black", state = "normal")
+def unfreeze_range(x, count):
+    if (count == -1):
 
+        for a in looper(x):
+            button_matrix[a[0]][a[1]].configure(bg="black", state = "normal")
 
+    else:
+        arr = looper(x)
+        print(looper(x))
+        chosen = random.sample(arr, 6)
+
+        for a in chosen:
+            button_matrix[a[0]][a[1]].configure(bg="black", state = "normal")
 
 
 def fix():
@@ -125,33 +142,54 @@ def fix():
     root.geometry('%dx%d' % (w+1,h+1))
 
 def create_all_options():
-    global option_matrix
+    global option_matrix1
+    global option_matrix2
 
-    option_matrix[0][0] = 0
-    option_matrix[0][1] = 1
-    option_matrix[0][2] = 1
-    option_matrix[1][0] = 2
-    option_matrix[1][1] = 3
-    option_matrix[1][2] = 3
-    option_matrix[2][0] = 4
-    option_matrix[2][1] = 5
-    option_matrix[2][2] = 5
-    option_matrix[3][0] = 6
-    option_matrix[3][1] = 7
-    option_matrix[3][2] = 7
+    option_matrix1[0][0] = 0
+    option_matrix1[0][1] = 1
+    option_matrix1[0][2] = 1
+    option_matrix1[1][0] = 2
+    option_matrix1[1][1] = 3
+    option_matrix1[1][2] = 3
+    option_matrix1[2][0] = 4
+    option_matrix1[2][1] = 5
+    option_matrix1[2][2] = 5
+    option_matrix1[3][0] = 6
+    option_matrix1[3][1] = 7
+    option_matrix1[3][2] = 7
+
+    option_matrix2[0][0] = 0
+    option_matrix2[0][1] = 1
+    option_matrix2[0][2] = 1
+    option_matrix2[1][0] = 2
+    option_matrix2[1][1] = 3
+    option_matrix2[1][2] = 3
+    option_matrix2[2][0] = 4
+    option_matrix2[2][1] = 5
+    option_matrix2[2][2] = 5
+    option_matrix2[3][0] = 6
+    option_matrix2[3][1] = 7
+    option_matrix2[3][2] = 7
     #print(option_matrix)
 
 
 
 def display_options(opts, root):
+    global radio_list
+    for b in radio_list:
+        if (b):
+            b.grid_forget()
 
     i= 0
     for a in opts:
-        tk.Radiobutton(root,
+        radb = tk.Radiobutton(root,
            text=a,
            padx = 20,
            variable=rad_val,
-           value=a).grid(row = i, column = 13)
+           value=a)
+        radb.grid(row = i, column = 13)
+        radio_list.append(radb)
+
 
         i = i+1
 
@@ -173,6 +211,10 @@ def change_value_matrix(r,c,val, b):
     global option_matrix
     global value_matrix
     global rounds
+    global round_start_player
+    global curr_player
+    global curr_options2
+    global curr_options1
 
     value_matrix[r][c] = rad_val.get()
     b.config(text = rad_val.get())
@@ -181,16 +223,84 @@ def change_value_matrix(r,c,val, b):
     #b.config(text = rad_val.get())
     change_sums(r,c)
     if (turn_count%6 == 0):
-        if (turn_count//6 >= 4):
-            freeze_all()
-
-        curr_options = option_matrix[turn_count//6]
-        display_options(curr_options, root)
+        display_new_round()
         rounds = rounds-1
         change_board()
+
+
+    else:
+        if (curr_player == 1):
+            print("curr_options1 is:")
+            print(curr_options1)
+            print("val to remove is:")
+            print(rad_val.get())
+            curr_options1.remove(rad_val.get())
+            print("curr_options1 is after removal:")
+            print(curr_options1)
+            #displying next players options
+            display_options(curr_options2, root)
+
+        else:
+            print("curr_options2 is:")
+            print(curr_options2)
+            print("val to remove is:")
+            print(rad_val.get())
+            curr_options2.remove(rad_val.get())
+            print("curr_options2 is after removal:")
+            print(curr_options2)
+            #displying next players options
+            display_options(curr_options1, root)
         rad_val.set(-1)
 
+
+    #switch players after every turn
+    if (curr_player == 1):
+        curr_player = 2
+    else:
+        curr_player = 1
+
+
     update_scores()
+
+def display_new_round():
+    global turn_count
+    global round_start_player
+    global curr_options
+    global curr_options1
+    global curr_options2
+    global root
+    global rounds
+    global rad_val
+
+
+    if (turn_count//6 >= 4):
+        freeze_all()
+
+    #assigning correct round_starter player since each round alternates
+    if (round_start_player == 2):
+        round_start_player = 1
+    else:
+        round_start_player = 2
+
+    curr_options = option_matrix1[turn_count//6]
+    curr_options1 = option_matrix1[turn_count//6]
+    curr_options2 = option_matrix2[turn_count//6]
+
+    if (round_start_player == 1):
+        display_options(curr_options1, root)
+    else:
+        display_options(curr_options2, root)
+
+
+    rad_val.set(-1)
+    print("did_setup")
+
+
+
+def switch_options():
+    global rad_val
+    played = rad_val.get()
+
 
 def change_sums(r,c):
     vert_labels[r].config(text=row_sum(r))
@@ -245,7 +355,7 @@ def col_sum(i):
 def change_board():
     global rounds
     freeze_all()
-    unfreeze_range(rounds-1)
+    unfreeze_range(rounds-1, 6)
 
 
 def temp():
@@ -262,10 +372,16 @@ def temp():
 def looper(x):
     arr = []
     for i in range (0+x,9-x):
-        arr.append([x,i])
-        arr.append([i,x])
-        arr.append([8-x,i])
-        arr.append([i,8-x])
+        if [x,i] not in arr:
+            arr.append([x,i])
+        if [i,x] not in arr:
+            arr.append([i,x])
+
+        if [8-x,i] not in arr:
+            arr.append([8-x,i])
+
+        if [i,8-x] not in arr:
+            arr.append([i,8-x])
 
     return arr
 
